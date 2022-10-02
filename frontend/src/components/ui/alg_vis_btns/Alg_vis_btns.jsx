490d887fd7aaf30a90b50aefe8b436a20c5fb20a
Orbel B <o.baghdasian@gmail.com>
2022-10-01 19:00:25 -0700

    added floating control's for AV & some buttons. Not fin

--- frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
+++ frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import alg_vis_stps from './alg_vis_stps'
+import Alg_vis_stps from './Alg_vis_stps'
 import './alg_btns.css'
+import {GrPlay} from 'react-icons/gr';
+import {GrPrevious} from 'react-icons/gr';
 const Alg_vis_btns = () => {
   return (
     <div className='alg_visualizer'>
-        <button class='alg_vis__btn_play'>Play</button>
-        <button class='alg_vis__btn_inData'>INPUT DATA</button>
-        <button class='alg_vis__btn_steps'>STEPS</button>
-        <button class='alg_vis__btn_prev'>Prev</button>
-        <alg_vis_stps/>
-        <button class='alg_vis__btn_next'>Next</button>
-        <button class='alg_vis__btn_like'>up</button>
-        <button class='alg_vis__btn_dislike'>down</button>
-        <button class=''>report</button>
+         {/* <Alg_vis_stps/> */}
+        <button class='alg_vis_btn_play'><GrPlay/></button>
+        <button class='alg_vis_btn_inData'>INPUT DATA</button>
+        {/* <button class='alg_vis_btn_steps'>STEPS</button> */}
+        <button class='alg_vis_btn_prev'><GrPrevious/></button>
+        <button class='alg_vis_btn_next'></button>
+        <button class='alg_vis_btn_like'></button>
+        <button class='alg_vis_btn_dislike'></button>
+        <button class='alg_vis_btn_report'></button>
 
     </div>
   )
