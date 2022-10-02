6e700c4439e13a25022f8fff24d99f33b7b13c02
Orbel B <o.baghdasian@gmail.com>
2022-10-01 19:32:32 -0700

    added another button & site no lomger functions on non fullscreen

--- frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
+++ frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
@@ -3,6 +3,7 @@ import Alg_vis_stps from './Alg_vis_stps'
 import './alg_btns.css'
 import {GrPlay} from 'react-icons/gr';
 import {GrPrevious} from 'react-icons/gr';
+import {GrNext} from 'react-icons/gr'
 const Alg_vis_btns = () => {
   return (
     <div className='alg_visualizer'>
@@ -11,7 +12,7 @@ const Alg_vis_btns = () => {
         <button class='alg_vis_btn_inData'>INPUT DATA</button>
         {/* <button class='alg_vis_btn_steps'>STEPS</button> */}
         <button class='alg_vis_btn_prev'><GrPrevious/></button>
-        <button class='alg_vis_btn_next'></button>
+        <button class='alg_vis_btn_next'><GrNext/></button>
         <button class='alg_vis_btn_like'></button>
         <button class='alg_vis_btn_dislike'></button>
         <button class='alg_vis_btn_report'></button>
