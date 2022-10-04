5a1e28f61ef2f2d75fba5446f5dae011a8bdc067
Orbel B <o.baghdasian@gmail.com>
2022-10-03 18:05:06 -0700

    fixed the floating control bar for alg vis, still need to fix butttons

--- frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
+++ frontend/src/components/ui/alg_vis_btns/Alg_vis_btns.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Alg_vis_stps from './alg_vis_stps'
 import './alg_btns.css'
 import {GrPlay} from 'react-icons/gr';
 import {GrPrevious} from 'react-icons/gr';
@@ -10,7 +9,10 @@ const Alg_vis_btns = () => {
          {/* <Alg_vis_stps/> */}
         <button class='alg_vis_btn_play'><GrPlay/></button>
         <button class='alg_vis_btn_inData'>INPUT DATA</button>
-        {/* <button class='alg_vis_btn_steps'>STEPS</button> */}
+        <button class='alg_vis_btn_steps'>STEPS</button>
+        <div>
+          x step of x
+        </div>
         <button class='alg_vis_btn_prev'><GrPrevious/></button>
         <button class='alg_vis_btn_next'><GrNext/></button>
         <button class='alg_vis_btn_like'></button>
