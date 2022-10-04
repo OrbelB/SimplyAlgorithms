c0136a004995ab241334ce7f75067f01a786ce76
Orbel B <o.baghdasian@gmail.com>
2022-10-04 02:55:15 -0700

    updated quiz module, only thing left is to adjus button spacing

--- frontend/src/components/ui/topic_quiz/Topic_quiz.jsx
+++ frontend/src/components/ui/topic_quiz/Topic_quiz.jsx
@@ -5,39 +5,43 @@ import cx from "classnames"
 
 const Topic_quiz = () => {
 
-  const oprtion_arr = ["option_a", "option_b", "option_c", "option_d"];
+  const option_arr = ["option A", "option B", "option C", "option D"];
   return (
     //This is the start of the container
-    <div className={cx(styles['quiz_topic'], 'continer-fluid')}>
-        <div className='grid p-2'>
+    <div className={cx(styles['quiz_topic_container'], 'continer-fluid')}>
+        <div className='grid p-4'>
           <div className='row'>
-              Test Your knowlege
+              <h2 className={cx(styles['topic_quiz_header'])}>Test Your knowlege</h2>
           </div>
-          <div className='row'>Q1 what is a cat?</div>
+          <div className='row'>
+            <h4 className={cx(styles['topic_quiz_header'])}>Q: What is a cat?</h4>
+          </div>
           <div className='row'>
             <div className='col'>
-              <div className='row'>
+              <div className='row '>
                 <div className='col'>
-                  <button className={cx(styles['quiz_buttons_select_a'])}></button>
+                  <button className={cx(styles['quiz_buttons_select_a'])}>{option_arr[0]}</button>
                 </div>
                 <div className='col'>
-                 <button className={cx(styles['quiz_buttons_select_b'])}></button>
+                 <button className={cx(styles['quiz_buttons_select_b'])}>{option_arr[1]}</button>
                 </div>
               </div>
               <div className='row'>
                 <div className='col'>
-                 <button className={cx(styles['quiz_buttons_select_b'])}></button>
+                 <button className={cx(styles['quiz_buttons_select_b'])}>{option_arr[2]}</button>
                 </div>
                 <div className='col'>
-                  <button className={cx(styles['quiz_buttons_select_a'])}></button>
+                  <button className={cx(styles['quiz_buttons_select_a'])}>{option_arr[3]}</button>
                 </div>
               </div>
             </div>
             <div className='col'>
-            <button className={cx(styles['quiz_buttons_cont'])}></button>
+              <button className={cx(styles['quiz_buttons_cont'])}>continue quiz</button>
             </div>
             <div className='row'>
-              <a className={cx(styles['quiz_info'])} href='https://developer.mozilla.org/en-US/' target="_blank">QUIZ NAME</a>
+              <div className='col col-lg'>
+                <a className={cx(styles['quiz_info'])} href='https://developer.mozilla.org/en-US/' target="_blank">QUIZ NAME</a>
+              </div>
             </div>
           </div>
         </div>
