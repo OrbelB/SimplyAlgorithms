b43410236b0215f557c9a3287b1ff80328cc8c17
Kevin Flores <fk278964@gmail.com>
2022-10-04 08:05:34 -0700

    moved the detail component to its own folder and changed to global var colors

--- frontend/src/pages/t_pages/TemplateTopicPage.jsx
+++ frontend/src/pages/t_pages/TemplateTopicPage.jsx
@@ -1,6 +1,6 @@
 import AlgoFrame from "../../components/ui/algo-frame/AlgoFrame";
 import Alg_vis_btns from "../../components/ui/alg_vis_btns/Alg_vis_btns";
-import Detail from "../../components/ui/Detail";
+import Detail from "../../components/ui/detail/Detail";
 import CodeSnippet from "../../components/ui/code-snippet/CodeSnippet";
 import Topic_quiz from "../../components/ui/topic_quiz/Topic_quiz";
 import CommentFrame from "../../components/ui/comment/CommentFrame";
