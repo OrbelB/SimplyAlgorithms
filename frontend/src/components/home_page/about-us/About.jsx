8995c66bc0a6c6398ea487a11daa3b47c2247a7a
JPW378 <joseph.wang.848@my.csun.edu>
2022-10-11 16:19:23 -0700

    Added rest of home page components, restyled categories

---
+++ frontend/src/components/home_page/about-us/About.jsx
@@ -0,0 +1,16 @@
+import "./About.css"
+
+export default function About() {
+    return (
+        <div className="aboutus">
+            <h1 className="head">ABOUT US</h1>
+            <br/><br/>
+            <p className="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+               incididunt ut labore et dolore magna aliqua. Vitae et leo duis ut diam quam nulla 
+               porttitor. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Malesuada fames 
+               ac turpis egestas sed tempus. Consectetur purus ut faucibus pulvinar elementum 
+               integer enim. Sit amet consectetur adipiscing elit duis. </p>
+            <div className="bottom"></div>
+        </div>
+    )
+}
