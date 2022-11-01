902d9cdeed9b6c921257c330cbd949d61b9a8d94
OrbelB <98566559+OrbelB@users.noreply.github.com>
2022-11-01 12:29:58 -0700

    Delete backend directory

--- backend/src/main/java/com/simplyalgos/backend/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.simplyalgos.backend.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
