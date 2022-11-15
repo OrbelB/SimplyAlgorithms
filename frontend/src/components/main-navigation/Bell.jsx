9f4ff0367f2efb1a408b0593787759cdc70c4326
Kevin Flores <fk278964@gmail.com>
2022-11-15 09:58:21 -0800

    added under construction pop up for bell icon on main navigation

--- frontend/src/components/main-navigation/Bell.jsx
+++ frontend/src/components/main-navigation/Bell.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import cx from "classnames";
-import styles from "./Bell.module.css"
+import styles from "./Bell.module.css";
+import image from "../../assets/under-construction.jpg";
 export default function Bell() {
     const [clickedBell, setClickedBell] = useState(false);
     const handleBellButtonClicked = () => {
@@ -8,8 +9,36 @@ export default function Bell() {
     }
     return (
         <>
-            {clickedBell ? <span onClick={handleBellButtonClicked} className={cx("bi bi-bell-fill", styles["custom-bell"])}></span> :
-                <span onClick={handleBellButtonClicked} className={cx("bi bi-bell", styles["custom-bell"])}></span>}
+            {clickedBell ? <span onClick={handleBellButtonClicked} data-bs-toggle="modal" data-bs-target="#featureunderconstruction" className={cx("bi bi-bell-fill", styles["custom-bell"])}></span> :
+                <span onClick={handleBellButtonClicked} data-bs-toggle="modal" data-bs-target="#featureunderconstruction" className={cx("bi bi-bell", styles["custom-bell"])}></span>}
+
+
+<div className="modal fade" id="featureunderconstruction" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div className={"text-center"}>
+            <div className="font text-align-center">
+                <h2>This Feature is under Construction!</h2>
+            </div>
+                <img 
+                    src={image}
+                    height="100%"
+                    width="100%"
+                    className="rounded-circle text-align-center"
+                    >
+                </img>
+            <div className="font text-center">
+                <h2>Come back at a later time.</h2>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
         </>
 
     );
