4f168a6b78350e8b09d910de5c277533add68c3e
PDJefferson <djefferson545@gmail.com>
2022-11-27 19:42:27 -0800

    fixed time zones issues from the be;revers last sol.

--- frontend/src/utilities/beautify-time.js
+++ frontend/src/utilities/beautify-time.js
@@ -1,13 +1,8 @@
 import { parseISO, formatDistanceToNow } from "date-fns";
-
 export function beautifyTime({ createdDate }) {
   if (createdDate) {
- 
-    // quick fix substract 8 hours to it
-    const dtDateOnly = new Date(createdDate);
-    dtDateOnly.setHours(dtDateOnly.getHours() - 8);   
-    console.log(dtDateOnly);
-    const date = parseISO(dtDateOnly.toISOString());
+    const date = parseISO(createdDate);
+    console.log(date);
     const timePassed = formatDistanceToNow(date);
     return `${timePassed} ago`;
   }
