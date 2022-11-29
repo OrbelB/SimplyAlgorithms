a9215afc8a6c02a048892082195187874d2dab2a
JPW378 <joseph.wang.848@my.csun.edu>
2022-11-28 19:48:56 -0800

    Removed 'Quiz' section css

--- frontend/src/services/Api/base.js
+++ frontend/src/services/Api/base.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { refreshAccessToken } from "../auth";
-const AWS_BASE_URL = "http://simplyalgosserver-env.eba-wmcrmidx.us-east-1.elasticbeanstalk.com:5000";
+const AWS_BASE_URL = "https://www.api.simplyalgorithms.com";
+
 
 const LOCALHOST_BASE_URL = "http://localhost:5000";
 const apiClient = axios.create({
