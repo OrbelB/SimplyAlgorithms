33e0f9e4bc5eecb1a425d0553c8d72dee016cff6
JPW378 <joseph.wang.848@my.csun.edu>
2022-11-28 19:10:43 -0800

    added title and background to Category section of Home page

--- frontend/src/components/home_page/categories/Categories.jsx
+++ frontend/src/components/home_page/categories/Categories.jsx
@@ -5,6 +5,7 @@ import TypeAlgorithms from "./TypeAlgorithms";
 export default function Categories({types_topics}) {
     return (
         <div className="wrap-collabsible">
+        <h1 className="head">CATEGORIES</h1>
             {
                 types_topics.map((type_topic, index) => (
                     <TypeAlgorithms key={nanoid()} type_topic={type_topic} index={index}/>
