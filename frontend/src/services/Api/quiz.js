c0740f1806fd813b0a214192b40f0e45ec6cc0b8
OrbelB <o.baghdasian@gmail.com>
2023-02-26 22:16:55 -0800

    created the api for quiz

--- frontend/src/services/Api/quiz.js
+++ frontend/src/services/Api/quiz.js
@@ -3,4 +3,132 @@ import { get, post, put, destroy } from './base';
 
 export const PUBLIC_ENDPOINT_ROUTE = '/quiz';
 
-export const quizEndpoints = {};
+export const quizEndpoints = {
+  singleQuizById: (id, jwtAccessToken) =>
+    get(`${PUBLIC_ENDPOINT_ROUTE}/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwtAccessToken}`,
+      },
+    }),
+  list: (page, size, filterBy) =>
+    get(`${PUBLIC_ENDPOINT_ROUTE}/list`, {
+      params: page,
+      size,
+      filterBy,
+    }),
+  // CHECK
+  listUserHistory: (userInfo, jwtAccessToken) =>
+    get(
+      `${PUBLIC_ENDPOINT_ROUTE}/user_history`,
+      {
+        page: userInfo?.page,
+        size: userInfo?.size,
+        userId: userInfo?.userId,
+        byQuiz: userInfo?.byQiz,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  deleteALLUserTakenQuizzes: (takeQuiz, jwtAccessToken) =>
+    post(
+      `${PUBLIC_ENDPOINT_ROUTE}/deleteALLUserTakenQuizzes`,
+      {
+        userId: takeQuiz?.userId,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  calculateAverageQuizScore: (quizDTO, jwtAccessToken) =>
+    get(
+      `${PUBLIC_ENDPOINT_ROUTE}/avgQuizScore`,
+      {
+        quizId: quizDTO?.quizId,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  getAllUserQuizScore: (takeQuizDTO, jwtAccessToken) =>
+    get(
+      `${PUBLIC_ENDPOINT_ROUTE}/userQuizScore`,
+      {
+        quizId: takeQuizDTO?.quizId,
+        userId: takeQuizDTO?.userId,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  createQuiz: (userDTO, quizDTO, quizQuestionDTO, jwtAccessToken) =>
+    post(
+      `${PUBLIC_ENDPOINT_ROUTE}/create`,
+      {
+        userDTO,
+        quizDTO,
+        quizQuestionDTO,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  deleteQuiz: (quizId, userId, jwtAccessToken) =>
+    destroy(
+      `${PUBLIC_ENDPOINT_ROUTE}/delete`,
+      {
+        quizId,
+        userId,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  updateFullQuiz: (userDTO, quizDTO, quizQuestionDTO, jwtAccessToken) =>
+    post(
+      `${PUBLIC_ENDPOINT_ROUTE}/update`,
+      {
+        userDTO,
+        quizDTO,
+        quizQuestionDTO,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+  submitQuiz: (takeQuizDTO, jwtAccessToken) =>
+    post(
+      `${PUBLIC_ENDPOINT_ROUTE}/submitQuiz`,
+      {
+        takeQuizDTO,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtAccessToken}`,
+        },
+      }
+    ),
+};
--- frontend/src/services/quiz.js
+++ frontend/src/services/quiz.js
@@ -1,4 +1,122 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 import { createAsyncThunk } from '@reduxjs/toolkit';
 // eslint-disable-next-line no-unused-vars
 import { quizEndpoints } from './Api/quiz';
+
+export const fetchSingleQuiz = createAsyncThunk(
+  'quiz/SingleQuiz',
+  async (quizId) => {
+    const response = await quizEndpoints.singleQuizById(quizId);
+    return response.data;
+  }
+);
+
+export const fetchQuizList = createAsyncThunk(
+  'quiz/list',
+  async (pageParams) => {
+    const { page, size, filterBy } = pageParams;
+    const response = await quizEndpoints.list(page, size, filterBy);
+    return response.data;
+  }
+);
+
+export const fetchUserHistory = createAsyncThunk(
+  'quiz/user_history',
+  async (passedParams) => {
+    const { userInfo, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints.listUserHistory(
+      userInfo,
+      jwtAccessToken
+    );
+    return response.data;
+  }
+);
+
+export const deleteAllUserHistory = createAsyncThunk(
+  'quiz/deleteALLUserTakenQuizzes',
+  async (passedParams) => {
+    const { takeQuiz, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints.deleteALLUserTakenQuizzes(
+      takeQuiz,
+      jwtAccessToken
+    );
+    return response.data;
+  }
+);
+
+export const calcAvgQuizScore = createAsyncThunk(
+  'quiz/avgQuizScore',
+  async (passedParams) => {
+    const { quizDTO, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints.calculateAverageQuizScore(
+      quizDTO,
+      jwtAccessToken
+    );
+    return response.data;
+  }
+);
+
+export const fetchAllUserQuizScore = createAsyncThunk(
+  'quiz/userQuizScore',
+  async (passedParams) => {
+    const { takeQuizDTO, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints.getAllUserQuizScore(
+      takeQuizDTO,
+      jwtAccessToken
+    );
+    return response.data;
+  }
+);
+
+export const createQuiz = createAsyncThunk(
+  'quiz/create',
+  async (passedParams) => {
+    const { userDTO, quizDTO, quizQuestionDTO, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints
+      .createQuiz(userDTO, quizDTO, quizQuestionDTO, jwtAccessToken)
+      .catch((error) => {
+        return error;
+      });
+    return response.data;
+  }
+);
+
+export const deleteQuiz = createAsyncThunk(
+  'quiz/delete',
+  async (passedParams) => {
+    const { quizId, userId, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints
+      .deleteQuiz(quizId, userId, jwtAccessToken)
+      .catch((error) => {
+        return error;
+      });
+    return response.data;
+  }
+);
+
+export const updateQuiz = createAsyncThunk(
+  'quiz/update',
+  async (passedParams) => {
+    const { userDTO, quizDTO, quizQuestionDTO, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints
+      .updateFullQuiz(userDTO, quizDTO, quizQuestionDTO, jwtAccessToken)
+      .catch((error) => {
+        return error;
+      });
+    return response.data;
+  }
+);
+
+export const submitQuiz = createAsyncThunk(
+  'quiz/submitQuiz',
+  async (passedParams) => {
+    const { takeQuizDTO, jwtAccessToken } = passedParams;
+    const response = await quizEndpoints
+      .submitQuiz(takeQuizDTO, jwtAccessToken)
+      .catch((error) => {
+        return error;
+      });
+    return response.data;
+  }
+);
