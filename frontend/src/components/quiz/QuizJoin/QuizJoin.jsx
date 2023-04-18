357b03a70ed66f92e3d52ece72e97143cbd97b71
OrbelB <o.baghdasian@gmail.com>
2023-04-18 01:24:54 -0700

    quiz stuff

--- frontend/src/components/quiz/QuizJoin/QuizJoin.jsx
+++ frontend/src/components/quiz/QuizJoin/QuizJoin.jsx
@@ -1,6 +1,7 @@
 import './QuizJoin.css';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { Avatar } from '@mui/material';
 import OptionsMenu from '../../options-menu';
 import { deleteQuiz } from '../../../services/quiz';
 import { quizActions } from '../../../store/reducers/quiz-slice';
@@ -33,10 +34,21 @@ export default function JoinQuiz({
     <div className="join-screen container-fluid vh-100 p-5">
       <div key={quiz.quizId} className="row">
         <div className="col-md-8">
-          <h1 className="qname text-center display-4 mb-5">
+          <h1 className="qname text-center display-4 mb-2">
             {quiz.title.toUpperCase()}
           </h1>
+          <p className="qname text-center mb-5">{quiz.description}</p>
           {/* <h5 className="qdescription m-5 text-left">{quizDTO.title}</h5> */}
+          <div>
+            <Avatar
+              sx={{ width: 250, height: 250 }}
+              src={quiz.picture}
+              className="mx-auto my-3 border-2 border border-info"
+              alt="quiz picture"
+              loading="lazy"
+              variant="rounded"
+            />
+          </div>
         </div>
         <div className="col-md-2 qinfo col-6 mb-3">
           <div className="text-left mb-2">
@@ -44,8 +56,10 @@ export default function JoinQuiz({
             <p className="fs-4">{amountOfQuestions}</p>
           </div>
           <div className="text-left">
-            <h5>Time Limit</h5>
-            <p className="fs-4">∞ minutes</p>
+            {/* <h5>Time Limit</h5>
+            <p className="fs-4">None</p> */}
+            <h5>Created By </h5>
+            <p className="fs-5"> {createdBy.username} </p>
           </div>
         </div>
         <div className="col-auto text-end">
--- frontend/src/components/quiz/QuizPreview/QuizPreview.jsx
+++ frontend/src/components/quiz/QuizPreview/QuizPreview.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import './QuizPreview.css';
 import { nanoid } from '@reduxjs/toolkit';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { Avatar } from '@mui/material';
 import { quizActions } from '../../../store/reducers/quiz-slice';
 import usePaginationWithInfiniteScroll from '../../../hooks/use-pagination';
 import { fetchQuizList } from '../../../services/quiz';
@@ -22,14 +24,55 @@ export default function QuizDB({ quizList, status }) {
     status,
   });
   const dispatch = useDispatch();
+
   if (status === 'success') {
     return (
       <div className="flex d-flex flex-wrap justify-content-around g-0 mt-5">
-        {quizList.map(({ quizId, title, score, createdBy }, index) => {
-          if (index + 1 === quizList.length) {
+        {quizList.map(
+          (
+            { quizId, title, description, picture, score, createdBy },
+            index
+          ) => {
+            if (index + 1 === quizList.length) {
+              description = description.slice(0, 256) + '...';
+              return (
+                <NavLink
+                  ref={lastElementChild}
+                  key={() => nanoid()}
+                  className="nav-hover"
+                  aria-current="page"
+                  to={quizId}
+                  onClick={() => dispatch(quizActions.resetData())}
+                >
+                  <div className="card quizpreview p-3 w-75 m-4">
+                    <div className="container-fluid">
+                      <div className="row">
+                        <h5 className="text-center display-5 mb-2">{title}</h5>
+                      </div>
+                      <div className="row">
+                        <p className="text-center">
+                          Created By {createdBy.username}
+                        </p>
+                        <p className="text-center">Max Score: {score}</p>
+                      </div>
+                      <div className="row">{description}</div>
+                      <div>
+                        <Avatar
+                          sx={{ width: 250, height: 250 }}
+                          src={picture}
+                          className="mx-auto my-3"
+                          alt="quiz picture"
+                          loading="lazy"
+                          variant="rounded"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </NavLink>
+              );
+            }
             return (
               <NavLink
-                ref={lastElementChild}
                 key={() => nanoid()}
                 className="nav-hover"
                 aria-current="page"
@@ -46,44 +89,7 @@ export default function QuizDB({ quizList, status }) {
                     </div>
                     <div className="row">
                       <div className="col-5">
-                        <CircularProgressbar
-                          value={score}
-                          text={`${score}%`}
-                          strokeWidth="10"
-                          styles={buildStyles({
-                            strokeLinecap: 'round',
-                            textSize: '18px',
-                            pathColor: `#0000FF`,
-                            textColor: 'black',
-                            trailColor: 'darkgray',
-                          })}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </NavLink>
-            );
-          }
-          return (
-            <NavLink
-              key={() => nanoid()}
-              className="nav-hover"
-              aria-current="page"
-              to={quizId}
-              onClick={() => dispatch(quizActions.resetData())}
-            >
-              <div className="card quizpreview p-3 w-75 m-4">
-                <div className="container-fluid">
-                  <div className="row">
-                    <h5>{title}</h5>
-                  </div>
-                  <div className="row">
-                    <p>Created By {createdBy.username}</p>
-                  </div>
-                  <div className="row">
-                    <div className="col-5">
-                      <CircularProgressbar
+                        {/* <CircularProgressbar
                         value={score}
                         text={`${score}%`}
                         strokeWidth="10"
@@ -94,14 +100,15 @@ export default function QuizDB({ quizList, status }) {
                           textColor: 'black',
                           trailColor: 'darkgray',
                         })}
-                      />
+                      /> */}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </NavLink>
-          );
-        })}
+              </NavLink>
+            );
+          }
+        )}
       </div>
     );
   }
